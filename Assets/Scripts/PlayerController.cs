abc1c419cb7144d3f461b83754ca7b04337df3c3
Kacper Świątek <kacperswiatek99@gmail.com>
2023-11-05 14:48:53 +0100

    #Add - Character movment and attack animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,10 @@ public class PlayerController : MonoBehaviour
 
     // References
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     // Movement
-    private Vector2 _movment;
+    private Vector2 _movement;
     private bool _isFacingRight = true;
 
     // Attack
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -34,7 +36,25 @@ public class PlayerController : MonoBehaviour
         {
             float horizontalInput = Input.GetAxisRaw("Horizontal");
             float verticalInput = Input.GetAxisRaw("Vertical");
-            _movment = new Vector2(horizontalInput, verticalInput);
+            _movement = new Vector2(horizontalInput, verticalInput);
+
+            // Flip character
+            if (horizontalInput < 0f && _isFacingRight == true)
+            {
+                Flip();
+            }
+            else if (horizontalInput > 0f && _isFacingRight == false)
+            {
+                Flip();
+            }
+        }
+
+        if (Input.GetButtonDown("Fire1") == true && _isAttacking == false)
+        {
+            _movement = Vector2.zero;
+            _rigidbody.velocity = Vector2.zero;
+            _animator.SetTrigger("Attack");
+            _isAttacking = true;
         }
     }
 
@@ -42,9 +62,33 @@ public class PlayerController : MonoBehaviour
     {
         if ( _isAttacking == false)
         {
-            float horizontalVelocity = _movment.normalized.x * speed;
-            float verticalVelocity = _movment.normalized.y * speed;
+            float horizontalVelocity = _movement.normalized.x * speed;
+            float verticalVelocity = _movement.normalized.y * speed;
             _rigidbody.velocity = new Vector2(horizontalVelocity, verticalVelocity);
         }
+    }
+
+    void LateUpdate()
+    {
+        _animator.SetBool("Idle", _movement == Vector2.zero);
+
+        // Animator
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            _isAttacking = true;
+        }
+        else
+        {
+            _isAttacking = false;
+        }
+    }
+
+
+    private void Flip()
+    {
+        _isFacingRight = !_isFacingRight;
+        float localScaleX = transform.localScale.x;
+        localScaleX = localScaleX * -1f;
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
     }
 }
